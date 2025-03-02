e68a58413de754ea3b0043ec17da11880ba6b004
mohammadSelimReza <srreza1999@gmail.com>
2025-03-02 09:55:36 +0600

    wsgi error solved

--- _core/wsgi.py
+++ _core/wsgi.py
@@ -4,7 +4,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings.production')
 
 application = get_wsgi_application()
 app = application
